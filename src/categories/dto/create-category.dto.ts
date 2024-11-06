ba073bb4389fe07c341f884cdd4d2ab7fbfd6ee3
rizee14 <herdiansyahrizki977@gmail.com>
2024-11-06 14:45:09 +0700

    fix(be):categories

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -2,11 +2,6 @@ import { IsNotEmpty, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCategoryDto {
-  @ApiProperty({ description: 'Image URL for the category', example: 'https://example.com/image.jpg' })
-  @IsNotEmpty()
-  @IsString()
-  img: string;
-
   @ApiProperty({ description: 'Title of the category', example: 'Technology' })
   @IsNotEmpty()
   @IsString()
