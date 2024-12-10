3e641fca439303d7dda265e96c5f9c9bad67acab
chico-jamsut <chicorpl123@gmail.com>
2024-12-10 18:30:19 +0700

    kelar

--- prisma/seeders/galleries.seed.ts
+++ prisma/seeders/galleries.seed.ts
@@ -4,22 +4,22 @@ export async function seedGalleries(prisma: PrismaClient) {
   await prisma.galleries.createMany({
     data: [
         {
-          "name": "Abyan",
-          "img": '/Screenshot (56)-1733111600177.png',
+          "name": "T-SHOP",
+          "img": '/Screenshot (51)-1733111600177.png',
           "desc" : "lorem",
         },
         {
-          "name": "Chico",
+          "name": "BIZZBUD",
           "desc" : "lorem",
-          "img": '/Screenshot (55)-1733111600177.png',
+          "img": '/Screenshot (52)-1733111600177.png',
         },
         {
-          "name": "Dewangga",
+          "name": "BACK2SCHOOL",
           "desc" : "lorem",
           "img": '/Screenshot (54)-1733111600177.png',
         },
         {
-          "name": "Rizki",
+          "name": "ZOZO GAMES",
           "desc" : "lorem",
           "img": '/Screenshot (53)-1733111600177.png',
         },
